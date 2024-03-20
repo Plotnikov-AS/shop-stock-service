00dd02cfa177c87fad21d05ae6a1a24a29671b26
Антон Плотников <anton.plotnikov@domrf.ru>
2024-03-20 18:12:14 +0300

    init service

---
+++ src/main/java/ru/mai/shop/ShopStockServiceApp.java
@@ -0,0 +1,12 @@
+package ru.mai.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopStockServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopStockServiceApp.class, args);
+    }
+}
